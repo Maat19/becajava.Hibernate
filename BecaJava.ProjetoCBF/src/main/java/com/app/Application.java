c991258e2e377f999c0a49d3d693b9e95ed65ea6
Mateus Maia Agre <mmaiaagr@everis.com>
2020-11-02 21:22:19 -0300

    first commit

---
+++ BecaJava.ProjetoCBF/src/main/java/com/app/Application.java
@@ -0,0 +1,59 @@
+package com.app;
+
+import java.util.Scanner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.app.service.ClubeService;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	private final ClubeService clubeService;
+
+	private Boolean system = true;
+
+	public Application(ClubeService clubeService) {
+		this.clubeService = clubeService;
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Scanner leitor = new Scanner(System.in);
+
+		while (system) {
+			System.out.println("Escolha uma opção:");
+			System.out.println("0 - Sair");
+			System.out.println("1 - Nome do Time");
+
+			int action = leitor.nextInt();
+			if (action == 1) {
+				clubeService.inicial(leitor);
+				
+				
+				
+			}
+			
+			if (action == 2) {
+				clubeService.exclusao(leitor);
+				
+				
+				
+			}
+			else {
+				
+				system = false;
+			}
+		}
+
+	}
+
+}
---
+++ BecaJava.ProjetoCBF/src/main/java/com/app/service/ClubeService.java
@@ -0,0 +1,72 @@
+package com.app.service;
+
+import java.util.Scanner;
+
+import org.springframework.stereotype.Service;
+
+import com.app.model.Clube;
+import com.app.repository.ClubeRepository;
+
+@Service
+public class ClubeService {
+	
+	private final ClubeRepository clubeRepository;
+	
+	public ClubeService(ClubeRepository clubeRepository) {
+		this.clubeRepository = clubeRepository;
+	}
+	
+	public void inicial(Scanner leitor) {
+		salvar(leitor);
+	}
+	
+	public void exclusao(Scanner leitor) {
+		excluir(leitor);
+	}
+	
+	private void salvar(Scanner leitor) {
+		System.out.println("Escreva o nome do time");
+
+		String nome = leitor.next();
+		Clube clube = new Clube();
+		System.out.println("Escreva numero de pontos");
+		int pontos = leitor.nextInt();
+		clube.setNome(nome);
+		clube.setPontos(pontos);
+		
+		clubeRepository.save(clube);
+		
+		System.out.println("Salvo");
+	}
+	
+	
+	private void excluir(Scanner leitor) {
+		System.out.println("Nome do id");
+
+		long id = leitor.nextLong();
+		Clube clube = new Clube();
+	
+		clube.setId(id);
+	
+		
+		clubeRepository.delete(clube);
+		
+		System.out.println("Excluido");
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
